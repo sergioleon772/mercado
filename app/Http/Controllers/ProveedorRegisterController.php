be902c09f5cd4aa2f8058443a5a66e458d747875
sergio <sergioleon10@outlook.es>
2025-04-10 23:08:31 -0400

    cam

--- app/Http/Controllers/ProveedorRegisterController.php
+++ app/Http/Controllers/ProveedorRegisterController.php
@@ -39,7 +39,7 @@ class ProveedorRegisterController extends Controller
 
     
     public function create() {
-        return view('Admin.proveedor.registrarprov'); // Vista que usará el admin
+        return view('Admin.proveedor.registrarProv'); // Vista que usará el admin
     }
     
     
